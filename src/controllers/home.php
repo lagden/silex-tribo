7c6b7d39da49c2b5af92656a66727723db642999
Thiago Lagden <lagden@gmail.com>
2013-07-05 16:01:43 -0300

    backend do twitter

--- src/controllers/home.php
+++ src/controllers/home.php
@@ -22,45 +22,22 @@ class home implements ControllerProviderInterface
         ->bind( 'mais' );
 
         $controllers
-        ->get( "/facebook-updates", [$this, 'facebook'] )
-        ->bind( 'facebook' );
+        ->get( "/twitter", [$this, 'twitter'] )
+        ->bind( 'twitter' );
 
         return $controllers;
     }
 
     public function index( Application $app )
     {
-        return $app['twig']->render( 'home/index.html.twig', [] );
+        $tweets = static::twitter();
+        return $app['twig']->render( 'home/index.html.twig', ['tweets'=>$tweets] );
     }
 
     public function mais( Application $app, $number )
     {
         $boxes = $app['twig']->render( 'home/partial/box.html.twig', ['number'=>$number] );
         $response = ["success"=>true, "html"=>$boxes];
-        return $app->json($response, 201);
-    }
-
-    public function facebook( Application $app )
-    {
-
-        // ID Da página que você quer o feed
-        $profile_id = "368231783220476";
-
-        // Quantidade de posts que você quer
-        $quantidade = 5;
-
-        // ID e App Secret do app no Facebook
-        $app_id = "201185700039693";
-        $app_secret = "cd73e7d48fb944dcf4b46960fff71870";
-
-        // $authToken = static::fetchUrl("https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&client_id={$app_id}&client_secret={$app_secret}");
-
-        // $json_object = json_decode(static::fetchUrl("https://graph.facebook.com/{$profile_id}/feed?{$authToken}&limit={$quantidade}"), true);
-
-        $json_object = json_decode( file_get_contents(__DIR__ . "/../tmp/facebook.json"), true );
-
-        $response = ["success"=>true, "data"=>$json_object];
-
         return $app->json($response, 201);
     }
 
@@ -72,18 +49,17 @@ class home implements ControllerProviderInterface
         return $app->redirect($_SERVER['HTTP_REFERER']);
     }
 
-    static private function fetchUrl($url)
+    static private function twitter()
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-        // You may need to add the line below
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+        $out = exec("curl --get 'https://api.twitter.com/1.1/statuses/user_timeline.json' --data 'contributor_details=false&count=2&exclude_replies=true&screen_name=tribo' --header 'Authorization: OAuth oauth_consumer_key=\"42T8fw5GWeqLesWQ3wNksA\", oauth_nonce=\"9ef0b7e07ac125cf87c56eea6b0b20b4\", oauth_signature=\"AckK7tMnlZ%2BsUBnr5thpLIoTpMU%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1373050111\", oauth_token=\"44358342-y48yiWS6Qf8t0CS0cylJrc25Jot03rxwctSJ0u0ax\", oauth_version=\"1.0\"'");
+        $tweets = json_decode($out, true);
+        $parseTweets = [];
+        foreach ($tweets as $k => $tweet)
+        {
+            $parseTweets[$k]['text'] = $tweet['text'];
+            $parseTweets[$k]['date'] = date("d/m/Y", strtotime($tweet['created_at']));
+        }
 
-        $feedData = curl_exec($ch);
-        curl_close($ch);
-
-        return $feedData;
+        return $parseTweets;
     }
 }
